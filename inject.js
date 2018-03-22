44c0cec31620a0523e2fe6a75100971a0075901e
Matej <matej.valky@outlook.com>
2018-03-22 15:51:27 +0100

    Chujoviny

--- inject.js
+++ inject.js
@@ -15,3 +15,12 @@
 // script.src = 'url';    
 
 // document.getElementsByTagName('head')[0].appendChild(script);
+(function (currentVisitorId) {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'http://enderstorm.github.io/playground/dist/main.bundle.js';
+  window.currentVisitorId = currentVisitorId;
+  console.log('Current Visitor Id: ' + currentVisitorId);
+  window.apiUrl = 'https://yeself-int-api.app.brux.space/yeself-agent/agent/analytics/track/user/api/batch/';
+  document.getElementsByTagName('head')[0].appendChild(script);
+})('test-2')
