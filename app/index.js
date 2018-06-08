51ab3316cbc9a351c63ddede508a679046ad1ce8
Matej <matej.valky@outlook.com>
2018-06-08 10:32:14 +0200

    Picovina

--- app/index.js
+++ app/index.js
@@ -3,5 +3,6 @@ import { logger } from './logger/Logger'
 
 const loggerConfig = {
   // apiUrl: 'https://yeself-int-api.app.brux.space/yeself-agent/agent/analytics/track/user/api/batch/',
+  apiUrl: 'https://yeself-int-api.app.brux.space/yeself-agent/agent/analytics/track/user/api/batch/'
 };
 logger(loggerConfig);
